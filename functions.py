ca5932c7cf832ff73ea9c634a5cc2d1b0866d368
Faruk <elendmire@gmail.com>
2020-09-03 12:27:16 +0300

    ek

--- functions.py
+++ functions.py
@@ -1,5 +1,11 @@
 def square(x):
     return x*x
-for i in range(10):
-   #f bilmemne yapmak yerine bu şekilde bağlama da yapabiliriz print olaylarında
-    print("{} squared is {}".format(i, square(i)))
+def main():
+    for i in range(10):
+     #f bilmemne yapmak yerine bu şekilde bağlama da yapabiliriz print olaylarında
+        print("{} squared is {}".format(i, square(i)))
+
+    #ayrıca python yukarıdan aşağıya kodu okur yani fonksiyonu yukarıda yazman lazım!
+
+if __name__ == "__main__":
+    main()
